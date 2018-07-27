5b2a52409e0dada464b0d03dc6e4c6b1dea62ac6
Alyse Bos <alyse.bos@gmail.com>
2018-07-26 19:38:33 -0700

    fixed errors with db url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://alyse:Zerg1ing@ds245901.mlab.com:45901/alysebos-blog-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://alyse:Zerg1ing@ds245901.mlab.com:45901/alysebos-blog-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://alyse2:Zerg1ing123@ds245901.mlab.com:45901/alysebos-blog-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://alyse2:Zerg1ing123@ds245901.mlab.com:45901/alysebos-blog-app';
 exports.PORT = process.env.PORT || 8080;

70bd96d63a0fb680465af866516ef7c26f17c066
Alyse Bos <alyse.bos@gmail.com>
2018-07-26 19:53:50 -0700

    yet more errors

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://alyse2:Zerg1ing123@ds245901.mlab.com:45901/alysebos-blog-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://alyse2:Zerg1ing123@ds245901.mlab.com:45901/test-alysebos-blog-app';
+exports.DATABASE_URL = process.env.DATABASE_URL;
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 exports.PORT = process.env.PORT || 8080;
